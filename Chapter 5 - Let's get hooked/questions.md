88cc2dea6d1b81a028e6d6dc36c94f292b6464b9
Singhnav13 <navjot131@gmail.com>
2023-07-22 19:33:53 +0530

    Few code changes in body.jsx

--- Chapter 5 - Let's get hooked/questions.md
+++ Chapter 5 - Let's get hooked/questions.md
@@ -2,7 +2,7 @@ Q1. What is Hooks in React?
 
 Q2. What is export and import in React?
 
-Q3. What arethe different ways of exporting components in React?
+Q3. What are the different ways of exporting components in React?
 
 Q4. Should we put hard coded data into components file?
 Ans. No, you should put all the hard coded data like logo URL in a seperate folder named utils/comon/config and add a new file named constants.js (you can use whatever name you want to give to the file)
@@ -12,4 +12,13 @@ Q5. What is react hooks?
 Q6. What is state in react?
 
 Q7. What is useState() anduseEffect()?
+
+Q8. What is the importance of config.js file?
+
+Q9. What is React Fiber and Reconciliation?
+
+Q10. What is Diff Algorithm?
+
+Q11. when react updates the UI?
+Ans. whenever the state gets updated, react re-renders the UI
 
--- Chapter 5 - Let's get hooked/src/components/Body.jsx
+++ Chapter 5 - Let's get hooked/src/components/Body.jsx
@@ -3,15 +3,32 @@
 
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../../utils/constants";
+import { useState } from "react";
 
 const Body = () => {
-    return (
+  const [ListOfRestaurants, setListOfRestaurant] = useState(restaurantList);
+
+  return (
+    <>
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {
+          const filteredList = ListOfRestaurants.filter(
+            {res} => res.data.avgRating > 4
+          );
+          setListOfRestaurant(filteredList);
+        }}>Top Rated Restaurant</button>
+        </div>
       <div className="restaurant-list">
         {restaurantList.map((restaurant) => {
-          return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
         })}
       </div>
-    );
-  };
+    </div>
+    </>
+  );
+};
 
-  export default Body;
+export default Body;
