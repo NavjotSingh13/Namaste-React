62bc02625650ed3aa28a336cefce0062a388d5ce
Singhnav13 <navjot131@gmail.com>
2023-08-11 13:18:09 +0530

    Added tailwind to few components

--- Chapter 10 - Jo Dikhta Hai, Vo Bikta Hai/src/components/Body.jsx
+++ Chapter 10 - Jo Dikhta Hai, Vo Bikta Hai/src/components/Body.jsx
@@ -40,19 +40,19 @@ const Body = () => {
   return listOfRestaurant.length === 0 ? <Shimmer /> : (
     <>
     <div className="body">
-      <div className="filter">
+      <div className="filter flex mx-6">
         <div className="search">
           <input type="text" classsName="search-box" value={searchText} onChange={(e)=>{
             setSearchText(e.target.value);
           }} />
-          <button onClick={()=>{
+          <button className="mx-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{
             const fileredRestaurant = restaurantList.filter((res)=>res.data.name.toLowerCase().includes(searchText.toLocaleLowerCase()) );
 
             setFilteredRestaurant(fileredRestaurant);
 
           }}>Search</button>
         </div>
-        <button className="filter-btn" onClick={() => {
+        <button className="mx-6 text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800" onClick={() => {
           const filteredList = listOfRestaurant.filter(
             (res) => res.data.avgRating > 4
           );
